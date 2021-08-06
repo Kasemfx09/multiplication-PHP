7350571f0a5ada9a9182672dc23f857c4d58da2c
manik <manik@gmail.com>
2021-08-06 10:24:45 +0600

    namta 1-10

--- namta.php
+++ namta.php
@@ -1,9 +1,9 @@
 <?php
 
-#### Q.13 - - ফর লুপ ইউজ করে ২,৩,৪,৫,৬,৭,৮,৯ এর নামতা রেডি করুন 
+#### Q.13 - - ফর লুপ ইউজ করে  ১,২,৩,৪,৫,৬,৭,৮,৯ এর নামতা রেডি করুন 
 $multiTab = "Multiplication Tables";
 echo "<strong style = \"font-size:30px; margin-left:75px;border-bottom: 3px solid green;\">{$multiTab} <br></strong>";
-for($i = 2; $i <= 10; $i++){  
+for($i = 1; $i <= 10; $i++){  
     if($i <= 10){
         echo "<br><span style = \"border-bottom: 3px solid black; font-size:20px; color: #150599; margin-left:150px\"> Multiplication of {$i} <br></span></br>";       
     };   
@@ -12,9 +12,4 @@ for($i = 2; $i <= 10; $i++){
         echo " <span style = \"margin-left:150px\">{$i} x {$m} = {$nam} </span><br>";        
     };  
 };
-
-
-
-
-
 ?>
